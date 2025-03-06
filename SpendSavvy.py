2682a94636c6cf1ba660f26f19d6ffa8bd968da2
Pasindu Dulanjana <pasindu@agrithmics.lk>
2025-03-07 00:18:34 +0530

    Implemented addExpense, ShowExpense and TotalExpense

--- SpendSavvy.py
+++ SpendSavvy.py
@@ -1,5 +1,22 @@
+def addExpense(expenses, desc, amount):
+    expenses.append({"description": desc, "amont":amount})
+    print(f"Added expenses: {desc}, Amount {amount}")
+
+def showExpense(expenses):
+    print("Expense : ")
+    for expense in expenses:
+        print(f" - {expense['description']} : {expense['amont']}")
+    print(f"\nTotal Expense : {getTotalExpense(expenses)}")
+
+def getTotalExpense(expenses):
+    sum = 0
+    for expense in expenses:
+        sum += expense['amont']
+    return 
+
 def main():
     print("Welcome to Budget app")
+    expenses = []
 
     while True:
         print("What would you like to do ?")
@@ -11,9 +28,11 @@ def main():
             try:
                 choice = int(input("Enter Your choice (1/2/3) : "))
                 if(choice == 1):
-                    print("1) Add an Expense")
+                    desc = input("\nEnter expense description : ")
+                    amount = float(input("Enter expense amount : "))
+                    addExpense(expenses, desc,amount)
                 elif(choice == 2):
-                    print("2) Show budget details")
+                    showExpense(expenses)
                 elif(choice == 3):
                     print("3) Exit")
                 else:
